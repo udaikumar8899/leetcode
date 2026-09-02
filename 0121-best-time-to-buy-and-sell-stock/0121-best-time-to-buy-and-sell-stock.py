3fc9761a7133382540efa4c0b235b88813636c8e
UDAI KUMAR <udaikumar8899@gmail.com>
2026-09-02 13:33:09 +0530

    Time: 66 ms (21.29%), Space: 28.5 MB (76.31%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n=len(prices)
+        min_price=prices[0]
+        ans=0
+
+        for i in range(1,n):
+            curr_profit=prices[i]-min_price
+            ans=max(curr_profit,ans)
+            min_price=min(min_price,prices[i])
+
+        return ans
